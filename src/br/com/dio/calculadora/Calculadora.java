04b184759d6c86ee935bc8f636a5d312e282e729
Milena Morais <milenamorais2017@hotmail.com>
2022-07-12 11:16:04 -0300

    add calc

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,42 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		float divisao = divisao(a,b);
+
+		
+		System.out.println("soma " + soma);
+		System.out.println("subtração " + subtracao);
+		System.out.println("multiplicacao " + multiplicacao);
+		System.out.println("divisão " + divisao);
+
+	}
+	public static int soma(int a,int b) {
+		return a + b;
+	}
+	public static int subtracao(int a,int b) {
+		return a - b;
+	}
+	public static float divisao(float a,float b) {
+		return a / b;
+	}
+	public static int multiplicacao(int a,int b) {
+		return a * b;
+	}
+
+}
